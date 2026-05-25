a7a2070365ad45eb4258cc28236f92d29b35a69a
AzureAD\AngeloMartelliCorsoI <hammersworkout@gmail.com>
2026-05-25 17:03:44 +0200

    aggiunto moltiplicazione e divisione

--- Hello.py
+++ Hello.py
@@ -9,3 +9,13 @@ def sottra(primo,secondo):
     return primo-secondo
 
 print(f"Risultato sottrazione: {sottra(4,2)}")
+
+def molti(primo,secondo):
+    return primo*secondo
+
+print(f"Risultato della moltiplicazione: {molti(2,3)}")
+
+def divi(primo,secondo):
+    return primo/secondo
+
+print(f"Risultato della divisione: {divi(10,2)}")
