e170b58520e8175a1bd0174574a19c2fe17f74cc
AzureAD\AngeloMartelliCorsoI <hammersworkout@gmail.com>
2026-05-25 16:54:48 +0200

    aggiunta somma e sottrazione

--- Hello.py
+++ Hello.py
@@ -1,1 +1,11 @@
 print("Hello World")
+
+def somma(primo,secondo):
+    return primo+secondo
+
+print(f"Risultato somma: {somma(1,2)}")
+
+def sottra(primo,secondo):
+    return primo-secondo
+
+print(f"Risultato sottrazione: {sottra(4,2)}")
