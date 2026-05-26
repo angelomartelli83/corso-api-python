2d12a31d05ab54b0c63f38ea178aa814beb2aae0
angelomartelli83 <hammersworkout@gmail.com>
2026-05-26 14:32:10 +0000

    aggiunto dizionario

--- Registro.py
+++ Registro.py
@@ -14,3 +14,21 @@ voti.insert(2,5)
 
 for p in voti:
     print(p)
+
+ciao = {
+    "nome": "web dev",
+    "studenti": [
+        { "nome": "luca", "id": 2}
+    ] 
+}
+
+corso = {
+
+    "nome": "Web Dev",
+    "studenti":[
+        {"nome": "Luca", "id": 1},
+        {"nome": "Mario","id": 2}
+    ]
+}
+
+print(corso["studenti"][1]["nome"])
