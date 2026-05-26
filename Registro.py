d8d828fa55f1c0a6fc4e140748754440cb24db18
angelomartelli83 <hammersworkout@gmail.com>
2026-05-26 13:59:47 +0000

    aggiunta insert e pop

--- Registro.py
+++ Registro.py
@@ -1,9 +1,16 @@
 voti=["1","2","3","4"]
 
-voti.append("5")
+voti.append("8")
 
 for p in voti:
     print("Voto registrato:")
     print(p)
 
 print("Voti terminati")
+
+print("elimino voto: " + voti.pop())
+
+voti.insert(2,5)
+
+for p in voti:
+    print(p)
